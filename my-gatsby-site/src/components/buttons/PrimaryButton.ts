fcfd94b64c4ba20e57d97f00c42f666a3ec07b12
talalaite <justina.talalaite@gmail.com>
2021-09-21 18:12:52 +0300

    button

--- my-gatsby-site/src/components/buttons/PrimaryButton.ts
+++ my-gatsby-site/src/components/buttons/PrimaryButton.ts
@@ -1,14 +1,14 @@
+import React from 'react';
 import styled from 'styled-components';
 
 import { Button } from './Button';
 
 export const PrimaryButton = styled(Button).attrs({
-  as: 'primaryButton',
+  as: 'button',
 })`
   box-shadow: 0px 16px 32px rgba(255, 155, 78, 0.24);
   font-weight: 700;
-  height: 3.5rem;
-  padding: 0.5rem 2.5rem;
+  text-align: center;
   margin-top: 0.5rem;
   text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.16);
   &:hover {
